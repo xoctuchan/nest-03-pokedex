16e35e888680204143adf13e542c114bbdfa25be
Juan Pablo Xoc Tuchán <xoctuchan@yahoo.es>
2022-12-01 15:17:31 -0600

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [ServeStaticModule .forRoot({
     rootPath: join (__dirname,'..','public'),
     }), 
     MongooseModule.forRoot('mongodb+srv://mern_user:UmNGmNKtyCJ7Y6Qa@cluster0.b0kdc.mongodb.net/pokedex'),
-    PokemonModule],
+    PokemonModule,
+    SeedModule],
   controllers: [],
   providers: [],
 })
